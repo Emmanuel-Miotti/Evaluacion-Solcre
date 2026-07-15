7df92bddf2e1da871aa0f3247d4e32c617b2e9c2
Emmanuel-Miotti <109709518+Emmanuel-Miotti@users.noreply.github.com>
2026-07-15 18:05:09 -0300

    Endpoint de login y logout para admin

---
+++ Backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Admin;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function Login (Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $admin = Admin::where('email', $credentials['email'])->first();
+
+          if (! $admin || ! Hash::check($credentials['password'], $admin->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['Las credenciales no son correctas.'],
+            ]);
+        }
+
+        $token = $admin->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Sesion cerrada']);
+    }
+
+}
